acf2187cf3e6d1eb648a1e1042c367efd137ddac
Yashwant <yashwantkrsingh1997@gmail.com>
2023-01-04 20:54:38 +0530

    04-01

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,57 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import data from '../data/products.json'
 import Card from 'react-bootstrap/Card';
+
+const myArray = [
+  { name: "cat", count: 0 },
+  { name: "dog", count: 0 },
+  { name: "hamster", count: 0 },
+  { name: "lizard", count: 0 }
+];
+
+function shuffle(arra1) {
+  var ctr = arra1.length,
+    temp,
+    index;
+  while (ctr > 0) {
+    index = Math.floor(Math.random() * ctr);
+    ctr--;
+    temp = arra1[ctr];
+    arra1[ctr] = arra1[index];
+    arra1[index] = temp;
+  }
+  return arra1;
+}
 
 
 const Home = () => {
 
   const [items, setItems] = useState(data);
   const [itemsoutofstock, setItemoutofstock] = useState();
-
   const [show, setShow] = useState(false);
+  let blueProduct, blackProduct, greenProduct, whiteProduct, yellowProduct, pinkProduct, orangeProduct, purpleProduct;
+
+  useEffect(() => {
+    // arrange the porduct based on color 
+
+    //const mountArray = shuffle(myArray);
+    divideDataOnColor();
+  }, [])
+
   const handleClick = () => {
     setShow(s => !s);
   };
 
 
-
-  //attemp 1
-  const filterOutofStock1 = () => {
-
-    const filterItem = data.map(
-      (data) =>
-        data.variants.filter((item) => {
-          return (item.inventory_quantity === 0)
-
-        })
-    )
-    console.log(filterItem);
-    setItemoutofstock(filterItem);
-
-
-
-  }
-  //attemp 2
-  const filterOutofStock12 = () => {
-
-    const results = data.find(comp =>
-      comp.variants.filter(inner =>
-        inner.inventory_quantity === '0'
-      ));
-    console.log(results);
-    //setItemoutofstock(results);
-
-
-
-  }
   //attemp 3
 
   function myFunction1() {
-    const searchField = "0";
     const test2 = data
       .map((data) => {
         let details = data.variants.filter((detail) =>
-        detail.inventory_quantity === 0
+          detail.inventory_quantity === 0
         );
         if (!details.length) {
           return null;
@@ -60,23 +58,65 @@ const Home = () => {
       })
       .filter(Boolean);
 
-    console.log("test2", test2);
+    console.log("outOfStockData", test2);
     setItemoutofstock(test2);
-
-
   };
-
 
   function myFunction() {
     handleClick();
-    // filterOutofStock1();
-    //filterOutofStock12();
     myFunction1();
 
   }
 
 
 
+  function divideDataOnColor() {
+
+    blueProduct = data.filter(l => {
+      return l.title.toLowerCase().match('blue');
+    });
+    console.log(blueProduct)
+
+    const blackProduct = data.filter(l => {
+      return l.title.toLowerCase().match('black');
+    });
+    // console.log(blackProduct)
+
+    const greenProduct = data.filter(l => {
+      return l.title.toLowerCase().match('green');
+    });
+    // console.log(greenProduct)
+
+    const whiteProduct = data.filter(l => {
+      return l.title.toLowerCase().match('white');
+    });
+    // console.log(whiteProduct)
+
+    const yellowProduct = data.filter(l => {
+      return l.title.toLowerCase().match('yellow');
+    });
+    //console.log(yellowProduct)
+
+    const pinkProduct = data.filter(l => {
+      return l.title.toLowerCase().match('pink');
+    });
+    //console.log(pinkProduct)
+
+    const orangeProduct = data.filter(l => {
+      return l.title.toLowerCase().match('orange');
+    });
+    //console.log(orangeProduct)
+
+    const purpleProduct = data.filter(l => {
+      return l.title.toLowerCase().match('purple');
+    });
+    //console.log(purpleProduct)
+
+    var products = [...blackProduct, ...greenProduct, ...blueProduct, ...whiteProduct, ...yellowProduct, ...pinkProduct, ...orangeProduct, ...purpleProduct];
+    //console.log(products);
+    setItems(products);
+  }
+  
 
   return (
     <>
@@ -85,9 +125,9 @@ const Home = () => {
       <div className='menu-tabs container'>
         <div className='menu-tab d-flex justify-content-around'>
 
-          <button className='btn btn-warning'>Red</button>
 
-          <button onClick={myFunction}>{show > 0 ? 'remove Out of Stock' : 'include out of stock' }</button>
+          <button >Suffle</button>
+          <button onClick={myFunction}>{show > 0 ? 'remove Out of Stock' : 'include out of stock'}</button>
 
 
         </div>
@@ -98,11 +138,11 @@ const Home = () => {
           <div className='col-11 mx-auto'>
             <div className='row my-5'>
               {items.map((elem) => {
-                const { id, title, image, variants } = elem;
+                const { id, title, image } = elem;
                 return (
                   <div className='item1 col-12 col-md-6 col-lg-6 col-xl-4 my-5' key={id}>
 
-                    <Card style={{ width: '17rem' }}>
+                    <Card style={{ width: '14rem' }}>
                       <Card.Header as="h5">Sale</Card.Header>
                       <Card.Img variant="top" src={image.src} />
                       <Card.Body>
@@ -123,28 +163,28 @@ const Home = () => {
 
               {show
                 ? <div className='row my-5 col-11 mx-auto'> {itemsoutofstock.map((elem) => {
-                const { id, title, image, variants } = elem;
-                return (
-                  <div className='item2 col-12 col-md-6 col-lg-6 col-xl-4 my-5' key={id}>
+                  const { id, title, image } = elem;
+                  return (
+                    <div className='item2 col-12 col-md-6 col-lg-6 col-xl-4 my-5' key={id}>
 
-                    <Card style={{ width: '17rem' }}>
-                      <Card.Header as="h5">Out of stock</Card.Header>
-                      <Card.Img variant="top" src={image.src} />
-                      <Card.Body>
-                        <Card.Title>{title}</Card.Title>
+                      <Card style={{ width: '17rem' }}>
+                        <Card.Header as="h5">Out of stock</Card.Header>
+                        <Card.Img variant="top" src={image.src} />
+                        <Card.Body>
+                          <Card.Title>{title}</Card.Title>
 
-                        {elem.variants.map(variants => {
-                          const { id, price } = variants;
-                          return (<Card.Text key={id}>{price}</Card.Text>)
+                          {elem.variants.map(variants => {
+                            const { id, price } = variants;
+                            return (<Card.Text key={id}>{price}</Card.Text>)
 
-                        })}
+                          })}
 
-                      </Card.Body>
-                    </Card>
-                  </div>
+                        </Card.Body>
+                      </Card>
+                    </div>
 
-                )
-              })}</div>
+                  )
+                })}</div>
                 : ''
               }
             </div>
